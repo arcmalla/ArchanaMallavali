84238e3f689db72e546f35b1126e478e8f665ad5
Archana MALLAVALI <arcmalla@WKMZT2B1D37A.global.publicisgroupe.net>
2022-08-05 14:17:45 +0530

    remove space

--- shopping_cart/src/components/footer.jsx
+++ shopping_cart/src/components/footer.jsx
@@ -1,7 +1,7 @@
 function Footer(){
     return(
         <div style={{padding:'10px', background:'lightgray',color:'white', marginTop:'10px'}}>
-            Copy right @ 2011-2018 Sabka bazar Grocery Supplies pvt. ltd
+            Copysright @ 2011-2018 Sabka bazar Grocery Supplies pvt. ltd
         </div>
     )
 }
